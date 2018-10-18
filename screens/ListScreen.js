45bd2d0f4c4d8fe272ce5efcec0a64b0bb458d40
trymhog97 <tehogeli@stud.ntnu.no>
2018-10-18 12:25:21 +0200

    Started on list using FlatList-component #2 #5

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, AsyncStorage } from 'react-native';
+import { List, ListItem} from 'react-native-elements';
+
+
 
 export default class ListScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            data:[]
+
+        };
+
+
+    }
+    componentDidMount(){
+        this.fetchDataRequest()
+    }
+
+    fetchDataRequest = () => {
+        const json = [{"location": "Trondheim", "lat": "2", "long": "3", "index":"2"},{"location":"Bergen", "lat":"31", "long":"43", "index":"1"}];
+        this.setState({data:json});
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>ListScreen</Text>
-            </View>
+            <List style={styles.container}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({item}) => (
+                        <ListItem
+                            title={item.location}
+                            subtitle={item.lat}
+
+                        />
+                    ) }
+                    keyExtractor={item => item.index}
+                />
+            </List>
         );
     }
 }
