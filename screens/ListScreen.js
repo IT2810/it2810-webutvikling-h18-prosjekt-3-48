0013bf3ceb6273f7a0ba08e7098168f86b50ebda
trymhog97 <tehogeli@stud.ntnu.no>
2018-10-19 15:32:15 +0200

    small commit to #5

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -7,60 +7,64 @@ import { List, ListItem,Button } from 'react-native-elements';
 export default class ListScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            data:[],
-            checked:false,
+        this.state = {
+            data: [],
+            checked: false,
 
         }
-        press = () =>{
+        press = () => {
             this.setState((state) => ({
                 checked: !state.checked,
             }));
         }
+
     }
 
 
-    componentDidMount(){
-        this.fetchDataRequest();
-    }
 
-    fetchDataRequest = () => {
-        const json = [{"location": "Trondheim", "lat": "2", "long": "3", "index":"2"},{"location":"Bergen", "lat":"31", "long":"43", "index":"1"
-        },{"location": "Oslo", "lat": "310", "long": "999", "index":"3"}];
-        this.setState({data:json});
-
+    getLocalPoints = async  () => {
+     AsyncStorage.getItem('POI: active').then((value) => {
+         var active=[];
+         if(value != null) {
+             active = JSON.parse(value);
+             this.setState({data: active});
+         }
+         console.log(active);
+     });
     }
 
 
     render() {
 
-    return (
-        <List style={styles.container}>
-            <FlatList
-                data={this.state.data}
-                renderItem={({item}) => (
-                    <ListItem
-                        title={item.location}
-                        subtitle={<View style={{flexDirection:'row', flexWrap:'wrap', alignItems:'center'}}>
-                            <Text>
-                                Latitude: {item.lat} Longitude: {item.long}
-                            </Text>
-                        <Button
-                            buttonStyle={styles.button}
-                            title="Mark as done"
-                            onPress={this.press}
-                        />
-                        <Button
-                            buttonStyle={styles.button}
-                            title="Remove"
-                            onPress={this.press}
-                        /></View>}
-                    />)}
-                keyExtractor={item => item.index}
-            />
-        </List>
-    );
-}}
+        return (
+            <List style={styles.container}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({item}) => (
+                        <ListItem
+                            title={item.description}
+                            subtitle={<View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center'}}>
+                                <Text>
+                                    Latitude: {item.coordinate.lat} Longitude: {item.coordinate.long}
+                                    {item.description}
+                                </Text>
+                                <Button
+                                    buttonStyle={styles.button}
+                                    title="Mark as done"
+                                    onPress={this.press}
+                                />
+                                <Button
+                                    buttonStyle={styles.button}
+                                    title="Remove"
+                                    onPress={this.press}
+                                /></View>}
+                        />)}
+                    keyExtractor={item => item.index}
+                />
+            </List>
+        );
+    }
+}
 
 
 const styles = StyleSheet.create({
