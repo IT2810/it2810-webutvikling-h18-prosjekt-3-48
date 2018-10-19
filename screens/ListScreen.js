05f33b42a0c007620a7e928d628450bdb7c36525
trymhog97 <tehogeli@stud.ntnu.no>
2018-10-19 12:19:41 +0200

    Added buttons to listscreen, needs more styling #5

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, AsyncStorage } from 'react-native';
-import { List, ListItem} from 'react-native-elements';
+import { StyleSheet, Text, View, FlatList, AsyncStorage} from 'react-native';
+import { List, ListItem,Button } from 'react-native-elements';
 
 
 
@@ -8,39 +8,59 @@ export default class ListScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            data:[]
+            data:[],
+            checked:false,
 
-        };
-
-
+        }
+        press = () =>{
+            this.setState((state) => ({
+                checked: !state.checked,
+            }));
+        }
     }
+
+
     componentDidMount(){
-        this.fetchDataRequest()
+        this.fetchDataRequest();
     }
 
     fetchDataRequest = () => {
         const json = [{"location": "Trondheim", "lat": "2", "long": "3", "index":"2"},{"location":"Bergen", "lat":"31", "long":"43", "index":"1"}];
         this.setState({data:json});
+
     }
+
 
     render() {
-        return (
-            <List style={styles.container}>
-                <FlatList
-                    data={this.state.data}
-                    renderItem={({item}) => (
-                        <ListItem
-                            title={item.location}
-                            subtitle={item.lat}
 
+    return (
+        <List style={styles.container}>
+            <FlatList
+                data={this.state.data}
+                renderItem={({item}) => (
+                    <ListItem
+                        title={item.location}
+                        subtitle={<View>
+                            <Text>
+                                Latitude: {item.lat} Longitude: {item.long}
+                            </Text>
+                        <Button
+                            buttonStyle={styles.button}
+                            title="Click me"
+                            onPress={this.press}
                         />
-                    ) }
-                    keyExtractor={item => item.index}
-                />
-            </List>
-        );
-    }
-}
+                        <Button
+                            buttonStyle={styles.button}
+                            title="Click me"
+                            onPress={this.press}
+                        /></View>}
+                    />)}
+                keyExtractor={item => item.index}
+            />
+        </List>
+    );
+}}
+
 
 const styles = StyleSheet.create({
     container: {
@@ -49,4 +69,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+
+    button: {
+        flex:2,
+        flexDirection: 'row',
+        width: 100,
+        height: 45,
+        backgroundColor: '#101010',
+        borderWidth:0,
+        borderRadius:5,
+        marginTop:5
+
+    }
 });
