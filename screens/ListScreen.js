96fe785ba7a45e001e8d449ffe515352a6e45489
trymhog97 <tehogeli@stud.ntnu.no>
2018-10-19 13:55:35 +0200

    added some more dummy data

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -25,7 +25,8 @@ export default class ListScreen extends React.Component {
     }
 
     fetchDataRequest = () => {
-        const json = [{"location": "Trondheim", "lat": "2", "long": "3", "index":"2"},{"location":"Bergen", "lat":"31", "long":"43", "index":"1"}];
+        const json = [{"location": "Trondheim", "lat": "2", "long": "3", "index":"2"},{"location":"Bergen", "lat":"31", "long":"43", "index":"1"
+        },{"location": "Oslo", "lat": "310", "long": "999", "index":"3"}];
         this.setState({data:json});
 
     }
@@ -40,18 +41,18 @@ export default class ListScreen extends React.Component {
                 renderItem={({item}) => (
                     <ListItem
                         title={item.location}
-                        subtitle={<View>
+                        subtitle={<View style={{flexDirection:'row', flexWrap:'wrap', alignItems:'center'}}>
                             <Text>
                                 Latitude: {item.lat} Longitude: {item.long}
                             </Text>
                         <Button
                             buttonStyle={styles.button}
-                            title="Click me"
+                            title="Mark as done"
                             onPress={this.press}
                         />
                         <Button
                             buttonStyle={styles.button}
-                            title="Click me"
+                            title="Remove"
                             onPress={this.press}
                         /></View>}
                     />)}
@@ -65,19 +66,19 @@ export default class ListScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'row',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
 
     button: {
-        flex:2,
-        flexDirection: 'row',
-        width: 100,
-        height: 45,
+        width: 30,
+        height: 30,
         backgroundColor: '#101010',
         borderWidth:0,
         borderRadius:5,
+        marginLeft: 20,
         marginTop:5
 
     }
